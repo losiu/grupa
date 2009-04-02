029b27489ae397a68a4bda7fd95337b6b8e9ddf3
Liorin <adamwojciech.witkowski@gmail.com>
2009-04-02 13:10:57 +0200

    Cannon template

---
+++ Cannon.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grupa;
+
+import static battlecode.common.GameConstants.*;
+import battlecode.common.*;
+
+/**
+ *
+ * @author liorin
+ */
+public class Cannon extends AbstractRobot implements RobotApi {
+    public Cannon(RobotController myRC) {
+        super(myRC);
+    }
+
+    public void nextTurn() {
+        
+    }
+
+}
