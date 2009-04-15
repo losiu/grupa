d8c73491dcc495d291a452a1a568bead3f36f068
Liorin <adamwojciech.witkowski@gmail.com>
2009-04-15 23:48:35 +0200

    Minor changes

--- AbstractRobot.java
+++ AbstractRobot.java
@@ -1,49 +1,63 @@
 package grupa;
 
 import battlecode.common.*;
+
 //import static battlecode.common.GameConstants.*;
 
-public abstract class AbstractRobot implements RobotApi{
+public abstract class AbstractRobot implements RobotApi {
 	
 	protected final RobotController myRC;
 	
 	public AbstractRobot(RobotController rc) {
 		myRC = rc;
+        
 	}
 	
 	boolean isOnMap (MapLocation mapLocation){
 		
-		if (!myRC.canSenseSquare(mapLocation))
-			return false;
-		
-		return (myRC.senseTerrainTile(mapLocation).getType()
+		/*if (!myRC.canSenseSquare(mapLocation))
+			return false;*/
+		TerrainTile tile = myRC.senseTerrainTile(mapLocation);
+        if (tile == null) {
+            return false;
+        }
+		return (tile.getType()
 				!= TerrainTile.TerrainType.OFF_MAP);
+        
 	}
 	
 	boolean isFreeLocation(MapLocation mapLocation) throws Exception{
 		
 		if (!myRC.canSenseSquare(mapLocation))
 			return false;
-		
-		return (myRC.senseGroundRobotAtLocation(mapLocation) == null);
+        try {
+            Robot r = myRC.senseGroundRobotAtLocation(mapLocation);
+            return (r == null);
+        }
+            catch (Exception e) {}
+            return false;
+        
 	}
 		
-	public void setDirectionToUnownedFluxDeposit() throws Exception{
+	public void setDirectionToUnownedFluxDeposit() {
 		
 		while (myRC.isMovementActive()){
 			myRC.yield();
 		}
 		
 		Direction direction = myRC.senseDirectionToUnownedFluxDeposit();
-		
-		if (direction != Direction.OMNI && myRC.getDirection() != direction){
-			myRC.setDirection(direction);
-			myRC.yield();
-		}
+		try {
+            if (direction != Direction.OMNI && direction != Direction.NONE && myRC.getDirection() != direction){
+                myRC.setDirection(direction);
+                myRC.yield();
+            }
+        } catch (Exception e) {}
 		
 	}
+
+
 	
-	public Robot[] senseEnemyRobots()  throws GameActionException{
+	public Robot[] senseEnemyRobots() {
 		
 		Robot[] allAirRobots = myRC.senseNearbyAirRobots();
 		
@@ -61,19 +75,20 @@ public abstract class AbstractRobot implements RobotApi{
 				robots = allGroundRobots;
 			
 			for (int i = 1; i < robots.length; i++){
-				
-				if (myRC.canSenseObject(robots[i])){
-				
-					RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
+				try {
+                    if (myRC.canSenseObject(robots[i])){
+
+                        RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
 					
-					if (robotInfo.team != myRC.getTeam()){
-						count++;
-					}
+                        if (robotInfo.team != myRC.getTeam()){
+                            count++;
+                        }
 					
-				}
+                    }
+                } catch (Exception e) {}
+
+            }
 					
-			}
-			
 		}
 		
 		Robot[] enemyRobots = new Robot[count];
@@ -88,18 +103,18 @@ public abstract class AbstractRobot implements RobotApi{
 				robots = allGroundRobots;
 			
 			for (int i = 1; i < robots.length; i++){
-				
-				if (myRC.canSenseObject(robots[i])){
-				
-					RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
+				try {
+                    if (myRC.canSenseObject(robots[i])){
+                        RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
 					
-					if (robotInfo.team != myRC.getTeam()){
-						enemyRobots[count] = robots[j];
-						j++;
-					}
+                        if (robotInfo.team != myRC.getTeam()){
+                            enemyRobots[count] = robots[j];
+                            j++;
+                        }
+                    
 					
-				}
-					
+                    }
+				} catch (Exception e) {}
 			}
 		}
 
@@ -108,8 +123,9 @@ public abstract class AbstractRobot implements RobotApi{
 	}
 	
 	
-	public Robot[] senseMyRobots(RobotType robotType) throws GameActionException{
-		
+	public Robot[] senseMyRobots(RobotType robotType) {
+		/*Poprzednia wersja wywalaala exception, gdy w czasie wykonania metody
+         jeden z robotow wyjechal poza zasieg*/
 		Robot[] allRobots;
 		
 		if (robotType.isAirborne()){
@@ -121,17 +137,17 @@ public abstract class AbstractRobot implements RobotApi{
 		int count = 0;
 		
 		for (int i = 1; i < allRobots.length; i++){
-			
-			if (myRC.canSenseObject(allRobots[i])){
-			
-				RobotInfo robotInfo = myRC.senseRobotInfo(allRobots[i]);
+			try{
+                if (myRC.canSenseObject(allRobots[i])){
+                
+                    RobotInfo robotInfo = myRC.senseRobotInfo(allRobots[i]);
 				
-				if (robotInfo.type == robotType && robotInfo.team == myRC.getTeam()){
-					count++;
-				}
-				
-			}
-				
+                    if (robotInfo.type == robotType && robotInfo.team == myRC.getTeam()){
+                        count++;
+                    }
+                
+                }
+			} catch (Exception e ) { }	
 		}
 		
 		Robot[] myRobots = new Robot[count];
@@ -139,43 +155,58 @@ public abstract class AbstractRobot implements RobotApi{
 		int j = 0;
 		
 		for (int i = 1; i < allRobots.length; i++){
-			
-			if (myRC.canSenseObject(allRobots[i])){
-			
-				RobotInfo robotInfo = myRC.senseRobotInfo(allRobots[i]);
+			try {
+                if (myRC.canSenseObject(allRobots[i])){
+                
+                    RobotInfo robotInfo = myRC.senseRobotInfo(allRobots[i]);
 				
-				if (robotInfo.type == robotType && robotInfo.team == myRC.getTeam()){
-					myRobots[j] = allRobots[i];
-					j++;
-				}
+                    if (robotInfo.type == robotType && robotInfo.team == myRC.getTeam()){
+                        myRobots[j] = allRobots[i];
+                        j++;
+                    }
+                } 
 				
-			}
+			} catch (Exception e){}
 				
 		}
+        /*Niwe wiadomo, ile naprawdę pól jest wypełnionych 
+         niektóre roboty mogły uciec z pola widzenia naszego robota
+         Dlatego tablicę wynikową trzeba uzupełnić*/
+        for (int i = j; i < allRobots.length; i++) {
+            myRobots[j] = null;
+        }
 		
 		return myRobots;
 		
 		
 	}
 	
-	public MapLocation[] senseMyRobotsLocations(RobotType robotType) throws Exception{
+	public MapLocation[] senseMyRobotsLocations(RobotType robotType) {
 		
 		Robot[] robots = senseMyRobots(robotType);
 		MapLocation[] robotsLocations = new MapLocation[robots.length];
 		
 		for (int i = 0; i < robots.length; i++) {
-			RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
-			robotsLocations[i] = robotInfo.location;
+            try {
+                RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
+                robotsLocations[i] = robotInfo.location;
+            } catch (Exception e) {
+                robotsLocations[i] = null;
+            }
 		}
 		
 		return robotsLocations;
 		
 	}
 	
-	public MapLocation findTheNearestArchon(Robot robot) throws Exception{
-		
-		RobotInfo robotInfo = myRC.senseRobotInfo(robot);
-		
+	public MapLocation findTheNearestArchon(Robot robot) {
+		RobotInfo robotInfo = null;
+        try {
+            robotInfo = myRC.senseRobotInfo(robot);
+            /*Jeśli tu będzie wyjątek, to znaczy, że badany robot jest poza zasięgiem
+             wogóle, nie wiem, czy nie jest sensowniej zrobić tą metode od MapLocation, a nie od Robota*/
+            
+        } catch (Exception e) {return null; }
 		MapLocation[] archons = myRC.senseAlliedArchons();
 		
 		boolean first = true;
@@ -199,12 +230,37 @@ public abstract class AbstractRobot implements RobotApi{
 	
 	}
 	
-	public void tryToMoveInDirection(Direction direction) throws Exception{
+    protected boolean move(Direction dir) {
+        
+        try {
+            while (myRC.hasActionSet() || myRC.isMovementActive()) {
+                myRC.yield();
+            }
+            if (myRC.getDirection() != dir) {
+                myRC.setDirection(dir);
+                myRC.yield();
+            }
+            myRC.moveForward();
+            myRC.yield();
+        } catch (Exception e) { return false;}
+
+        return true;
+    }
+
+	public void tryToMoveInDirection(Direction direction) {
 		
-		while (myRC.isMovementActive()){
-			myRC.yield();
-		}
-		
+	
+        if (!move(direction) ) {
+            /*Nie jestem pewien, jaki byl pierwotny zamysl, ale na razie niech bedzie tak*/
+            try {
+                if (myRC.hasActionSet()) {
+                    myRC.yield();
+                }
+                myRC.setDirection(myRC.getDirection().rotateRight());
+            } catch (Exception e) {}
+        }
+		/* To nie wygląda na poprawny kod
+         * przecież, jeśli zmienimy kierunek na direction, to już się nie ruszamy
 		if (myRC.canMove(direction) && direction != myRC.getDirection()) {
 			myRC.setDirection(direction);
 		} else {
@@ -215,36 +271,39 @@ public abstract class AbstractRobot implements RobotApi{
 			}
 					
 		}
+         */
 	
 		myRC.yield();
 		
 	}
 	
-	public int chooseRobotToTransferEnergon(Robot[] robots) throws Exception{
+	public int chooseRobotToTransferEnergon(Robot[] robots) {
 		
 		// id of the robot with minimal energon level
+        /*lepiej -1 dać na początek, łatwiej błąd poznać*/
 		int idRobot = robots.length;
 			
 		double minEnergon = 0;
 			
 		for (int i = 0; i < robots.length; i++) {
-				
-			if (myRC.canSenseObject(robots[i])){
+			try {
+                if (myRC.canSenseObject(robots[i])){
 					
-				RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
+                    RobotInfo robotInfo = myRC.senseRobotInfo(robots[i]);
 
-				if (myRC.getLocation().isAdjacentTo(robotInfo.location)){
+                    if (myRC.getLocation().isAdjacentTo(robotInfo.location)){
 					
-					double energon = robotInfo.eventualEnergon;
+                        double energon = robotInfo.eventualEnergon;
 						
-					if (energon < minEnergon || idRobot == robots.length){
-						minEnergon = energon;
-						idRobot = i;
-					}
+                        if (energon < minEnergon || idRobot == robots.length){
+                            minEnergon = energon;
+                            idRobot = i;
+                    	}
 			
-				}
+                    }
 					
-			}
+                }
+            } catch (Exception e) {}
 			
 		}
 		
@@ -287,6 +346,30 @@ public abstract class AbstractRobot implements RobotApi{
 		return direction;
 		
 	}
+
+
+    /*Tutaj proponuje umieścić metody odpowiedzialne za ocenianie możliwości robota
+     np. liczenie ile energonu jest potrzebne żeby dojść z jednej lokacji do drugiej, itp.*/
+    protected double travelCost(MapLocation start, MapLocation end) {
+        /*@return Zwraca minimalną ilość energonu, który jest potrzebny
+         aby przejść najkrótszą drogą z start do end
+         Uwaga: ta metoda nie uwzględnia różnic w wysokości terenu!*/
+        RobotType type = myRC.getRobotType();
+        /*najpierw obliczamy jaki jest całkowity koszt ruchu o jedno pole po skosie i prosto
+          to  +1 jest konieczne, bo trzeba wliczyć sam ruch */
+        double diagonalMoveCost = type.energonUpkeep() * (type.moveDelayDiagonal() +1);
+        double orthogonalMoveCost = type.energonUpkeep() * (type.moveDelayOrthogonal() + 1);
+
+        int distX = Math.abs(start.getX() - end.getX());
+        int distY = Math.abs(start.getY() - end.getY());
+        /*tyle będzie ruchów po skosie  - tyle, ile potrzeba by zniwelować różnicę na współrzędnej x lub y,
+         zależnie od tego gdzie ta różnica jest mniejsza*/
+        int diagonalMovesNeeded = Math.min(distX, distY);
+        /*potem trzeba będzie wykonać resztę ruchów w kierunkach głównych*/
+        int orthogonalMovesNeeded = Math.max(distX, distY) - diagonalMovesNeeded;
+        /**/
+        return (diagonalMovesNeeded*diagonalMoveCost + orthogonalMoveCost*orthogonalMovesNeeded);
+    }
 
 	
 		
--- Scout.java
+++ Scout.java
@@ -12,12 +12,78 @@ import battlecode.common.*;
  * @author liorin
  */
 public class Scout extends AbstractRobot implements RobotApi{
+    private enum Rotation {LEFT, RIGHT};
+    ScoutStatus state = ScoutStatus.START;
+    Direction patrolDirection;
+    Rotation patrolRotation = Rotation.RIGHT;
+    /*Na chwilę obecną mam takie pomysły zastosowania Scouta :
+     1) patrolowanie okolicy fluxa
+     2) polowanie na archony przeciwnika*/
     public Scout(RobotController myRC) {
         super(myRC);
     }
 
+
+    private void changeRotation() {
+        switch (patrolRotation) {
+            case RIGHT:
+                patrolRotation = Rotation.LEFT;
+                break;
+            case LEFT:
+                patrolRotation = Rotation.RIGHT;
+                break;
+        }
+    }
+
+    private Direction newPatrolDirection(Direction dir) {
+        switch (patrolRotation) {
+            case RIGHT:
+                return dir.rotateRight().rotateRight();
+            case LEFT:
+            default:
+                return dir.rotateLeft().rotateLeft();
+        }
+    }
+
+
+    private void readMessages() {
+        /*Czytamy wszystkie wiadomości, które przyszły
+         Powinniśmy czytać wszystkie, bo czytanie po jednej może spowodować duże opóźnienia i
+         trudności w synchronizacji działań robotów. */
+
+        for (Message m : myRC.getAllMessages()) {
+
+        }
+    }
+
     public void nextTurn() {
-    
+        readMessages();
+        switch (state){
+            case START:
+                /*po prostu czekamy na rozkazy*/
+                myRC.yield();
+                break;
+            case PATROL:
+                /*krótki opis:
+                 1. sprawdz, czy sa wrogowie w poblizu (nie-workery, bo workery są niegroźne)
+                 2. jeśli tak, wróć do fluxa
+                 3. jeśli nie, sprawdź czy masz dość energonu
+                 4. jeśli nie, wróć do fluxa
+                 5. jeśli tak, kontynuuj patrol*/
+
+                if (senseEnemyRobots().length != 0) {
+                }
+                
+                if (!move(patrolDirection)) {
+                    changeRotation();
+                }
+
+                break;
+            case FIND_ARCHONS:
+                break;
+            case KILL_ARCHON:
+                break;
+        }
     }
 
 }
